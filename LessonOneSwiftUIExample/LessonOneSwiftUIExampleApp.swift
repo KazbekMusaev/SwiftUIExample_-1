7ce76e8c1e1128604c4fde5858fb98c34ad50d09
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2024-02-21 01:22:16 +0300

    Initial Commit

---
+++ LessonOneSwiftUIExample/LessonOneSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonOneSwiftUIExampleApp.swift
+//  LessonOneSwiftUIExample
+//
+//  Created by apple on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LessonOneSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
